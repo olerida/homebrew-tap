67850d1b8305187bcc16ecd35a3171e1fa3f4ee6
Òscar Lérida <5617885+olerida@users.noreply.github.com>
2026-06-17 11:03:24 +0200

    Add Settle cask

---
+++ Casks/settle.rb
@@ -0,0 +1,11 @@
+cask "settle" do
+  version "1.0.0"
+  sha256 "aec70170fe8831e002ea46ac837a506ddbe684394161a0c9c9ead3fd7dead1e4"
+
+  url "https://github.com/olerida/Settle/releases/download/v1.0.0/Settle.dmg"
+  name "Settle"
+  desc "Guarda y restaura layouts de ventanas en macOS"
+  homepage "https://github.com/olerida/Settle"
+
+  app "Settle.app"
+end
