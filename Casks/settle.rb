9cd45cf1f445b668dd681669f991b3766983cb2e
Òscar Lérida <5617885+olerida@users.noreply.github.com>
2026-06-19 18:34:10 +0200

    Update settle cask to v1.3.1

--- Casks/settle.rb
+++ Casks/settle.rb
@@ -1,6 +1,6 @@
 cask "settle" do
-  version "1.3.0"
-  sha256 "65b90249034f9281cd3418d92f35a4468d0398ca483c5d78c959dde8b7c4a181"
+  version "1.3.1"
+  sha256 "c55b065c5cd97e7d3b7b4258ce872340ec5f105799ab84c25791446638fb52f8"
 
   url "https://github.com/olerida/Settle/releases/download/v#{version}/Settle.dmg"
   name "Settle"
