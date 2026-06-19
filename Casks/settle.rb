adf6d87a663f5fabc655ab4479ef4d2814d7aa98
Òscar Lérida <5617885+olerida@users.noreply.github.com>
2026-06-19 20:07:30 +0200

    Update settle cask to v1.4.0

--- Casks/settle.rb
+++ Casks/settle.rb
@@ -1,11 +1,14 @@
 cask "settle" do
-  version "1.3.1"
-  sha256 "c55b065c5cd97e7d3b7b4258ce872340ec5f105799ab84c25791446638fb52f8"
+  version "1.4.0"
+  sha256 "4a1da66eddef9adc22adb5a1ddd1ab6b98e56c0fcc57c39c5da525cd4b8520ba"
 
-  url "https://github.com/olerida/Settle/releases/download/v#{version}/Settle.dmg"
+  url "https://github.com/olerida/Settle/releases/download/v#{version}/Settle.dmg",
+      verified: "github.com/olerida/Settle/"
   name "Settle"
-  desc "Save and restore macOS window layouts"
-  homepage "http://settle.titanolandia.es"
+  desc "Save and restore window layouts from the menu bar"
+  homepage "http://settle.titanolandia.es/"
+
+  depends_on macos: :sonoma
 
   app "Settle.app"
 end
