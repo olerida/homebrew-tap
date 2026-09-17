c7d884100512008ecf517b1e3aa8c13e6a11c552
Òscar Lérida <5617885+olerida@users.noreply.github.com>
2026-09-17 11:23:30 +0200

    Update AI Usage MB to 1.4.0

--- Casks/aiusage.rb
+++ Casks/aiusage.rb
@@ -1,6 +1,6 @@
 cask "aiusage" do
-  version "1.3.0"
-  sha256 "aef5335a3e0bc9284f12a7b46ab4cd37ba192c6d52420d359f0dcc97cf99ec43"
+  version "1.4.0"
+  sha256 "a5b7f1ede214ffd15f9757644e345321265c82cb66a58048b9fe19e4934f004b"
 
   url "https://github.com/olerida/AIusage/releases/download/v#{version}/AIusage-macos-universal.zip"
   name "AI Usage MB"
